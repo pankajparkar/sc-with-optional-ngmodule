c9e6c04aa0aee9d644e3aac3dcf24e806245f56c
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-10-04 08:45:31 +0800

    Removed html and scss files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sc-root',
-  template: `<h1>Hello {{ title }}</h1>`,
+  template: `
+    <h1>Hello {{ title }}</h1>
+    <router-outlet></router-outlet>
+  `,
   styles: [``]
 })
 export class AppComponent {
