10eeff5d44d246f0f1aa8d5cfcfb23f6572c6496
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-10-04 16:52:31 +0800

    Added role details page.

--- src/app/roles/roles.module.ts
+++ src/app/roles/roles.module.ts
@@ -6,10 +6,14 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { RoleDetailsComponent } from './details/details.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 const matModules = [
   MatTableModule,
   MatCardModule,
+  MatButtonModule,
+  MatDividerModule,
 ];
 
 @NgModule({
