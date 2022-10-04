3347111d4e516a224904b241d129b44e498d73e2
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-10-04 08:47:36 +0800

    Removed AppRoutingModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot([]),
     BrowserAnimationsModule
   ],
   providers: [],
