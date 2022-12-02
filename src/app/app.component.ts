f8b0628fdd6d1b9e35bdd33d8d23a5f75412d763
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-03 04:18:39 +0800

    converted app component to SFC.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <sc-navbar></sc-navbar>
+  <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
 
